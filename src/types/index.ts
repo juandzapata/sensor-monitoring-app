5036e35a313604eed44c6e8c2f1eab531278d8a6
juanza <juan.zapata22871@ucaldas.edu.co>
2026-06-07 13:41:00 -0500

    feat(tpyes): create interfaces

---
+++ src/types/index.ts
@@ -0,0 +1,36 @@
+export interface Sensor {
+  id: number;
+  nombre: string;
+  tipo: string;
+  fabricante: string;
+  fecha_fabricacion: string;
+}
+
+export interface Zone {
+  id: number;
+  nombre: string;
+  descripcion: string;
+  ubicacion: string;
+  estado_operativo: string;
+}
+
+export interface Monitoring {
+  id: number;
+  sensor_id: number;
+  zone_id: number;
+  fecha_instalacion: string;
+  tipo_lectura: string;
+  valor_umbral: number;
+  valor_actual: number | null;
+  estado_monitoreo: string;
+}
+
+export interface MonitoringCreate {
+  sensor_id: number;
+  zone_id: number;
+  fecha_instalacion: string;
+  tipo_lectura: string;
+  valor_umbral: number;
+  valor_actual?: number;
+  estado_monitoreo: string;
+}
